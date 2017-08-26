35ca23c9bcf9bf66740e644e00081af1e5e47fcc
blankfrack <blankfrack@users.noreply.github.com>
2017-08-26 00:20:45 -0400

    Update Test.java
    GitHub MASTER MASTER

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -15,7 +15,7 @@ public class Test {
 		
 		System.out.println("Should be in the branch");
 
-		System.out.println("MASTER");
+		System.out.println("MASTER MASTER");
 		System.out.println("From NetBeans");
 
 		System.out.println("GITHUB 5 !!!");
