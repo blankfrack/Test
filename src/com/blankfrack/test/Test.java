2f268d7cbd3c1fa4ec579a4281ab6ba16b125750
Blankfrack <bryan.benoit@gmail.com>
2017-08-25 20:47:09 -0400

    Adding Test.java
    Initial commit

---
+++ src/com/blankfrack/test/Test.java
@@ -0,0 +1,17 @@
+package com.blankfrack.test;
+
+/**
+ * @author Bux
+ *
+ */
+public class Test {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!!");
+	}
+
+}
