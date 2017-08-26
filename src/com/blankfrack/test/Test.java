e4df328f70815aa26acd04cebb037934904646ce
Blankfrack <bryan.benoit@gmail.com>
2017-08-26 12:32:08 -0400

    Big Cleanup for the September Release

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -9,27 +9,8 @@ public class Test {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// Doing nothing
-		System.out.println("From IntelliJ Clean");
-		displayMessage();
-		displayAnotherMessage("Test message GITHUB");
-		displayEclipseMessage();
-	}
-
-	/**
-	 * 
-	 */
-	public static void displayMessage(){
-		System.out.println("DisplayMessage");		
-	}
-	
-	public static void displayAnotherMessage(String pMsg) {
-		System.out.println(pMsg);
-	}
-
-	public static void displayEclipseMessage() {
-		System.out.println("Eclipse");
+	public static void main(String[] args) {		
+		System.out.println("Hello World !!!");	
 	}
 	
 }
