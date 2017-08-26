bb3ed586e517589cec22086a29af9a40771afa10
Blankfrack <bryan.benoit@gmail.com>
2017-08-26 11:44:57 -0400

    Add client class

---
+++ src/com/blankfrack/test/Client.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.blankfrack.test;
+
+/**
+ * @author Bux
+ *
+ */
+public class Client {
+	
+	private String name;
+
+	/**
+	 * 
+	 */
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
