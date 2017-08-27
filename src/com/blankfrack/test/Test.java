a66b2ec20808c443030f1e4c81d116c0c0da7480
Blankfrack <bryan.benoit@gmail.com>
2017-08-27 14:17:24 -0400

    Reformat

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -9,8 +9,8 @@ public class Test {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {		
-		System.out.println("Hello World !!!");	
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
 	}
-	
+
 }
