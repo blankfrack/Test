5ef45438e2d44206f019e42e8693f423b82de561
blankfrack <blankfrack@users.noreply.github.com>
2017-08-27 15:13:00 -0400

    Cleanup once again

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -11,12 +11,6 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World !!!");
-		
-		
-		
-		
-		
-		
 		System.out.println("Test from Eclipse 2");
 	}
 
