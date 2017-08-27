513a5f2eb390aa2ac4ac5ede927e4fae04029ed2
Blankfrack <bryan.benoit@gmail.com>
2017-08-27 15:15:40 -0400

    Fix conflicts

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -11,12 +11,9 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World !!!");
-<<<<<<< HEAD
 		
 		System.out.println("Test from Eclipse 1");
-	
-=======
->>>>>>> branch 'master' of https://github.com/blankfrack/Test.git
+
 		System.out.println("Test from Eclipse 2");
 		
 		System.out.println("Test from Eclipse 3");
