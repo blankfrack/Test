5593de8c7214ed29a7bee5acfa9936c255edd1be
Blankfrack <bryan.benoit@gmail.com>
2017-08-27 15:13:54 -0400

    Add traces in Eclipse

--- src/com/blankfrack/test/Test.java
+++ src/com/blankfrack/test/Test.java
@@ -12,12 +12,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World !!!");
 		
-		
-		
-		
-		
-		
+		System.out.println("Test from Eclipse 1");
+	
 		System.out.println("Test from Eclipse 2");
+		
+		System.out.println("Test from Eclipse 3");
 	}
 
 }
